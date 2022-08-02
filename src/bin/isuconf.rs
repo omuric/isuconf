0212aeea30c24ef8c57174e616686643ff020afa
omu <omurice11@gmail.com>
2022-08-03 05:57:24 +0900

    list を omit

--- src/bin/isuconf.rs
+++ src/bin/isuconf.rs
@@ -1,5 +1,4 @@
 use anyhow::Result;
-use isuconf::commands::list::{list, ListOpt};
 use isuconf::commands::pull::{pull, PullOpt};
 use isuconf::commands::push::{push, PushOpt};
 use isuconf::commands::ssh::{ssh, SshOpt};
@@ -9,8 +8,6 @@ use structopt::StructOpt;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "isuconf")]
 enum Opt {
-    /// View config list
-    List(ListOpt),
     /// Pull configs from remote
     Pull(PullOpt),
     /// Push configs to remote
@@ -25,7 +22,6 @@ enum Opt {
 async fn main() -> Result<()> {
     let opt: Opt = Opt::from_args();
     match opt {
-        Opt::List(opt) => list(opt).await,
         Opt::Pull(opt) => pull(opt).await,
         Opt::Push(opt) => push(opt).await,
         Opt::Ssh(opt) => ssh(opt).await,
--- src/commands.rs
+++ src/commands.rs
@@ -1,4 +1,3 @@
-pub mod list;
 pub mod pull;
 pub mod push;
 pub mod ssh;
